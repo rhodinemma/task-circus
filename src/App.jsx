835b6ff730dc9e305b1eb3c18595eaef7844fe08
mosesmulumba <mulumbamoses94@gmail.com>
2023-01-26 21:14:28 +0300

    mo(new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,102 @@
+//import pic from "../assets/profile.jpeg"
 import "./App.css";
-
 
 function App() {
   return (
     <div className="App">
-      <div className="container mt-5 pb-5">
-        <center>
-          <h1 className="mb-4 pb-2" style={{borderBottom:"1px solid #ccc"}}>
-            <i class="fas fa-layer-group"></i> &nbsp;<b>Task Circus</b>
-          </h1>
-        </center>
+      <div className="container mt-3 pb-5">
+        <h1 className="mb-4 pb-2" style={{ borderBottom: "1px solid #ccc" }}>
+          <i className="fas fa-layer-group"></i> &nbsp;<b>Task Circus</b>
+        </h1>
 
-        <div className="col-lg-12 card-deck">
-          <div className="row" >
-            <div className="col-md-3 bgc-#ccc card-header card" style={{borderRadius:"0.7rem"}}>
-              <h1 className="card--header" style={{borderRadius:"0.7rem"}}>
-                <b>To Do</b></h1>
-                <div className="cards">
-                <div className="card border mb-2" style={{ width:"16rem", borderRadius:"0.5rem"}}>
+        <div className="col-12 card-deck">
+          <div className="row">
+            <div
+              className="col-sm-4 bgc-#ccc card-header card"
+              style={{ borderRadius: "0.7rem" }}
+            >
+              <h1
+                className="card-header mb-3"
+                style={{ borderRadius: "0.7rem" }}
+              >
+                <b>To Do</b>
+              </h1>
+              <div className="cards">
+                <div
+                  className="card border mb-2"
+                  style={{ width: "20rem", borderRadius: "0.5rem" }}
+                >
                   <div className="card-body border-bottom p-3">
                     <h5 className="card-title">
                       Custom Domain for learning apps
                     </h5>
                     <small
                       className="bg-danger p-1 text-white"
-                      style={{ borderRadius: "0.5rem"}}
+                      style={{ borderRadius: "0.5rem" }}
                     >
                       Jan 30
                     </small>
+                    <div className="text-end">
+                      <img
+                        src={moses}
+                        alt="dp"
+                        className="rounded-circle"
+                        width="30px"
+                        height="30px"
+                      />
+                      <img
+                        src={rhodin}
+                        alt="dp"
+                        className="rounded-circle"
+                        width="30px"
+                        height="30px"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="card border"
+                  style={{ width: "20rem", borderRadius: "0.5rem" }}
+                >
+                  <div
+                    className="card-body border-bottom"
+                    style={{ borderRadius: "0.7rem" }}
+                  >
+                    <h5 className="card-title">
+                      Increase Testing Coverage and make logging run
+                      asynchronously
+                    </h5>
+                    <small
+                      className="bg-danger p-1 text-white"
+                      style={{ borderRadius: "0.5rem" }}
+                    >
+                      Jan 21
+                    </small>
+                    <div className="text-end">
+                        <img src="/profile.jpeg" alt="dp" className="rounded-circle" width="10px" height="10px" />
+                    </div>
                   </div>
                   <div class="card-footer">
                     <small class="text-muted">Last updated 3 mins ago</small>
                   </div>
                 </div>
-                <div className="card border" style={{ width:"16rem", borderRadius:"0.5rem"}}>
-                  <div className="card-body border-bottom" style={{borderRadius:"0.7rem"}}>
-                    <h5 className="card-title">
-                      Increase Logging Coverage and make logging run asynchronously
-                    </h5>
-                    <small
-                      className="bg-danger p-1 text-white"
-                      style={{ borderRadius: "0.5rem"}}
-                    >
-                      Jan 21
-                    </small>
-                    <div className="text-end">
-                        <img src="/profle.jpeg" alt="dp" className="rounded-circle" width="30px" height="30px" />
-                    </div>
-                  </div>
-                  <div className="card-footer">
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                  </div>
-                </div>
               </div>
-            </div>&nbsp;&nbsp;&nbsp;&nbsp;
-            <div className="col-md-3 card-header card" style={{borderRadius:"0.7rem"}}>
-              <h1>
+            </div>
+            <div
+              className="col-sm-4 card-header card"
+              style={{ borderRadius: "0.7rem" }}
+            >
+              <h1
+                className="card-header mb-3"
+                style={{ borderRadius: "0.7rem" }}
+              >
                 <b>Doing</b>
               </h1>
 
-              <div className="card border border-1" style={{ width: "16rem" , borderRadius:"0.5rem"}}>
+              <div
+                className="card border border-1"
+                style={{ width: "20rem", borderRadius: "0.5rem" }}
+              >
                 <div className="card-body">
                   <h5 className="card-title">User Onboarding</h5>
                   <small
@@ -69,17 +106,26 @@ function App() {
                     Jan 30
                   </small>
                 </div>
-                <div className="card-footer">
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
               </div>
-            </div>&nbsp;&nbsp;&nbsp;&nbsp;
-            <div className="col-md-3 card-header card" style={{borderRadius:"0.7rem"}}>
-              <h1>
+            </div>
+            <div
+              className="col-sm-4 card-header card"
+              style={{ borderRadius: "0.7rem" }}
+            >
+              <h1
+                className="card-header mb-3"
+                style={{ borderRadius: "0.7rem" }}
+              >
                 <b>Completed</b>
               </h1>
 
-              <div className="card border border-1" style={{ width: "16rem" , borderRadius:"0.5rem"}}>
+              <div
+                className="card border border-1"
+                style={{ width: "20rem", borderRadius: "0.5rem" }}
+              >
                 <div className="card-body p-3">
                   <h5 className="card-title">
                     Implement manage user section on the admin-user profile
@@ -91,11 +137,11 @@ function App() {
                     Jan 30
                   </small>
                 </div>
-                <div className="card-footer">
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                <div class="card-footer">
+                    <small class="text-muted">Last updated 3 mins ago</small>
                 </div>
               </div>
-            </div>&nbsp;&nbsp;&nbsp;&nbsp;
+            </div>
           </div>
         </div>
       </div>
