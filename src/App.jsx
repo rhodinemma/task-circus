31b6285bc74cde15c117db0f39c90b26cf962efd
rhodinemma <rhodinemma10@gmail.com>
2023-01-30 09:23:24 +0300

    ft(make-task-data-dynamic)

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,50 @@
-import { useState , useEffect} from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import ToDo from "./components/sectionOne/ToDo";
 import Doing from "./components/sectionTwo/Doing";
 import Completed from "./components/sectionThree/Completed";
 
-
 const todos = [
   {
     title: "Custom Domain for learning apps",
     description: "Setup custom domain for learning apps",
+    date: "Jan 31",
   },
   {
     title: "Increase Testing Coverage and make logging run asynchronously",
     description: "Aim to increase the coverage to 80%",
+    date: "Feb 2",
+  },
+  {
+    title: "Integrate User Activity logs",
+    description: "User activity logs should be reported at the frontend",
+    date: "Feb 10",
   },
 ];
 
 function App() {
-  const [loading , setLoading ] = useState(true);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     setTimeout(() => {
-        setLoading(false);
+      setLoading(false);
     }, 3500);
-}, [])
+  }, []);
   return (
-      <>{loading ? 
+    <>
+      {loading ? (
         <center>
-          <h3 className="mt-5 pt-5 text-center"><b>loading..................</b></h3>
+          <h3 className="mt-5 pt-5 text-center">
+            <b>loading..................</b>
+          </h3>
         </center>
-        :
+      ) : (
         <>
           <div className="App">
             <div className="container mt-2 pb-5">
-              <h1 className="mb-4 pb-2" style={{ borderBottom: "1px solid #ccc" }}>
+              <h1
+                className="mb-4 pb-2"
+                style={{ borderBottom: "1px solid #ccc" }}
+              >
                 <i className="fas fa-layer-group"></i> &nbsp;<b>Task Circus</b>
               </h1>
 
@@ -48,8 +60,8 @@ function App() {
             </div>
           </div>
         </>
-        }
-      </>
+      )}
+    </>
   );
 }
 
--- src/components/sectionOne/ToDo.jsx
+++ src/components/sectionOne/ToDo.jsx
@@ -3,7 +3,8 @@ import moses from "../../assets/moses.jpeg";
 import rhodin from "../../assets/rhodin.jpg";
 import TaskModal from "../modal/index";
 
-const ToDo = () => {
+const ToDo = ({ data }) => {
+  console.log(data);
   const [modalShow, setModalShow] = useState(false);
   return (
     <>
@@ -14,88 +15,90 @@ const ToDo = () => {
         <h1 className="card-header mb-3" style={{ borderRadius: "0.7rem" }}>
           <b>To Do</b>
         </h1>
-        <div className="cards">
-          <div
-            className="card border mb-2"
-            style={{ width: "20rem", borderRadius: "0.5rem" }}
-          >
-            <div
-              className="card-body border-bottom p-3"
-              onClick={() => setModalShow(true)}
-            >
-              <h5 className="card-title">Custom Domain for learning apps</h5>
-              <div className="bottom--part mt-4 text-end">
-                <small
-                  className="bg-danger p-1 text-white mt-2"
-                  style={{ borderRadius: "0.5rem", float: "left" }}
+        {data.map((todo, index) => (
+          <>
+            <div className="cards">
+              <div
+                className="card border mb-2"
+                style={{ width: "20rem", borderRadius: "0.5rem" }}
+                key={index}
+              >
+                <div
+                  className="card-body border-bottom p-3"
+                  onClick={() => setModalShow(true)}
                 >
-                  Jan 30
-                </small>
-                <img
-                  src={moses}
-                  alt="dp"
-                  className="rounded-circle mt-2"
-                  width="30px"
-                  height="30px"
-                />
-                <img
-                  src={rhodin}
-                  alt="dp"
-                  className="rounded-circle mt-2"
-                  width="30px"
-                  height="30px"
-                />
+                  <h5 className="card-title">{todo.title}</h5>
+                  <div className="bottom--part mt-4 text-end">
+                    <small
+                      className="bg-danger p-1 text-white mt-2"
+                      style={{ borderRadius: "0.5rem", float: "left" }}
+                    >
+                      {todo.date}
+                    </small>
+                    <img
+                      src={moses}
+                      alt="dp"
+                      className="rounded-circle mt-2"
+                      width="30px"
+                      height="30px"
+                    />
+                    <img
+                      src={rhodin}
+                      alt="dp"
+                      className="rounded-circle mt-2"
+                      width="30px"
+                      height="30px"
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-
-          <div
-            className="card border"
-            style={{ width: "20rem", borderRadius: "0.5rem" }}
-            onClick={() => setModalShow(true)}
+          </>
+        ))}
+        <div className="mt-1 text-end">
+          <button
+            type="button"
+            className="mb-1 w-75 h-2 text-muted"
+            style={{
+              borderRadius: "0.3rem",
+              maxHeight: "auto",
+              borderStyle: "none",
+              float: "left",
+            }}
           >
-            <div
-              className="card-body border-bottom"
-              style={{ borderRadius: "0.7rem" }}
+            <i
+              className="fas fa-plus mt-1 text-muted"
+              style={{ float: "left" }}
+            ></i>
+            Add Task
+          </button>
+          <div className="symbols" style={{ float: "left" }}>
+            <i
+              className="material-icons text-muted"
+              data-bs-toggle="tooltip"
+              data-bs-html="true"
+              title="assignments"
             >
-              <h5 className="card-title">
-                Increase Testing Coverage and make logging run asynchronously
-              </h5>
-              <div className="bottom--part mt-4 text-end">
-                <small
-                  className="bg-danger p-1 text-white mt-2"
-                  style={{ borderRadius: "0.5rem", float: "left" }}
-                >
-                  Jan 30
-                </small>
-                <img
-                  src={moses}
-                  alt="dp"
-                  className="rounded-circle mt-2"
-                  width="30px"
-                  height="30px"
-                />
-                <img
-                  src={rhodin}
-                  alt="dp"
-                  className="rounded-circle mt-2"
-                  width="30px"
-                  height="30px"
-                />
-              </div>
-            </div>
+              assignment
+            </i>
+            <span
+              className="material-symbols-outlined"
+              data-bs-toggle="tooltip"
+              data-bs-html="true"
+              title="recommend"
+            >
+              recommend
+            </span>
+            <span
+              className="material-symbols-outlined"
+              data-bs-toggle="tooltip"
+              data-bs-html="true"
+              title="group_work"
+            >
+              group_work
+            </span>
           </div>
         </div>
-        <div className="mt-1 text-end">
-            <button type="button" className="mb-1 w-75 h-2 text-muted" style={{ borderRadius: "0.3rem" , maxHeight: "auto" ,borderStyle:"none" , float:"left"}}><i className="fas fa-plus mt-1 text-muted" style={{float:"left"}}></i> 
-              Add Task
-            </button>
-            <div className="symbols" style={{float:"left"}}>
-              <i className="material-icons text-muted" data-bs-toggle="tooltip" data-bs-html="true" title="assignments">assignment</i>
-              <span className="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-html="true" title="recommend">recommend</span>
-              <span className="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-html="true" title="group_work">group_work</span>
-            </div>
-         </div>
       </div>
 
       <TaskModal show={modalShow} onHide={() => setModalShow(false)} />
