89c7ed526abe46d180612cab9c9a674bb7ccee23
mosesmulumba <mulumbamoses94@gmail.com>
2023-02-04 16:20:27 +0300

    mo(add task functionality)

--- src/components/sectionThree/Completed.jsx
+++ src/components/sectionThree/Completed.jsx
@@ -1,12 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import moses from "../../assets/moses.jpeg";
 import rhodin from "../../assets/rhodin.jpg";
-import {Modal} from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 
-const Completed = ({data}) => {
-  console.log(data);
-  const [completedModal,setCompletedModal] = useState(false);
-  const handleCompletedClose= ()=> setCompletedModal(false);
+const Completed = ({ data }) => {
+  const [tasktitle, setTaskTitle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [inputModal, setInputModal] = useState(false);
+  const handleInputModalClose = () => setInputModal(false);
+
+  // function to submit a new task
+  const submitTask = () => {
+    // this adds a new task to our todos array
+    data.push({
+      title: tasktitle,
+      description: taskDescription,
+      date: "Feb 22",
+    });
+
+    // after adding a new task, close the input modal
+    setInputModal(false);
+  };
 
   return (
     <>
@@ -17,18 +31,16 @@ const Completed = ({data}) => {
         <h1 className="card-header mb-3" style={{ borderRadius: "0.7rem" }}>
           <b>Completed</b>
         </h1>
-        {data.map((completed , index)=>(
+        {data.map((completed, index) => (
           <>
             <div
               className="card border border-1 mb-2"
               style={{ width: "20rem", borderRadius: "0.5rem" }}
               key={index}
-              onClick={()=>setCompletedModal(true)}
+              onClick={() => setCompletedModal(true)}
             >
               <div className="card-body p-3">
-                <h5 className="card-title">
-                  {completed.title}
-                </h5>
+                <h5 className="card-title">{completed.title}</h5>
                 <div className="bottom--part mt-4 text-end">
                   <small
                     className="bg-dark p-1 text-white mt-2"
@@ -40,57 +52,92 @@ const Completed = ({data}) => {
                     {completed.date}
                   </small>
                   <img
-                      src={moses}
-                      alt="dp"
-                      className="rounded-circle mt-2"
-                      width="30px"
-                      height="30px"
-                      data-bs-toggle="tooltip"
-                      data-bs-html="true"
-                      title="mosesmulumba"
-                    />
-                    <img
-                      src={rhodin}
-                      alt="dp"
-                      className="rounded-circle mt-2"
-                      width="30px"
-                      height="30px"
-                      data-bs-toggle="tooltip"
-                      data-bs-html="true"
-                      title="rhodinemma"
-                    />
+                    src={moses}
+                    alt="dp"
+                    className="rounded-circle mt-2"
+                    width="30px"
+                    height="30px"
+                    data-bs-toggle="tooltip"
+                    data-bs-html="true"
+                    title="mosesmulumba"
+                  />
+                  <img
+                    src={rhodin}
+                    alt="dp"
+                    className="rounded-circle mt-2"
+                    width="30px"
+                    height="30px"
+                    data-bs-toggle="tooltip"
+                    data-bs-html="true"
+                    title="rhodinemma"
+                  />
                 </div>
               </div>
             </div>
-            </>
-            
-          ))}
-            <div className="mt-1 text-end">
-                <button type="button" className="mb-1 w-75 h-2 text-muted" style={{ borderRadius: "0.3rem" , maxHeight: "auto" ,borderStyle:"none" , float:"left"}}><i className="fas fa-plus mt-1 text-muted" style={{float:"left"}}></i> 
-                  Add Task
-                </button>
-                <div className="symbols" style={{float:"left"}}>
-                  <i className="material-icons text-muted" data-bs-toggle="tooltip" data-bs-html="true" title="assignments">assignment</i>
-                  <span className="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-html="true" title="recommend">recommend</span>
-                  <span className="material-symbols-outlined" data-bs-toggle="tooltip" data-bs-html="true" title="group_work">group_work</span>
-                </div>
+          </>
+        ))}
+        <div className="mt-1 text-end">
+          <button
+            type="button"
+            className="mb-1 w-100 h-2 text-muted"
+            style={{
+              borderRadius: "0.3rem",
+              maxHeight: "auto",
+              borderStyle: "none",
+              float: "left",
+            }}
+            onClick={() => setInputModal(true)}
+          >
+            <i
+              className="fas fa-plus mt-1 text-muted"
+              style={{ float: "left" }}
+            ></i>
+            Add Task
+          </button>
+        </div>
+        <Modal
+          keyboard={false}
+          show={inputModal}
+          onHide={handleInputModalClose}
+          style={{ minHeight: "15rem" }}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <h2 className="mt-3">Add task</h2>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="mb-3">
+              <label for="exampleFormControlInput1" className="form-label">
+                Task Title
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                onChange={(e) => setTaskTitle(e.target.value)}
+              />
             </div>
-          </div>
-          <Modal
-                keyboard={false}
-                show={completedModal} 
-                onHide={handleCompletedClose}
-              >
-              <Modal.Header closeButton>
-              <Modal.Title><h2 className='mt-3'>Completed Section</h2></Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <h3>Implement manage user section on the admin-user profile</h3>
-                <p>Add the disable and delete button</p>
-
-              </Modal.Body>
-          </Modal>
-
+            <div className="mb-3">
+              <label for="exampleFormControlTextarea1" className="form-label">
+                Task Description
+              </label>
+              <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+                onChange={(e) => setTaskDescription(e.target.value)}
+              ></textarea>
+            </div>
+            <button
+              className="mt-3 btn btn-primary"
+              onClick={() => submitTask()}
+            >
+              Submit task
+            </button>
+          </Modal.Body>
+        </Modal>
+      </div>
+      
     </>
   );
 };
